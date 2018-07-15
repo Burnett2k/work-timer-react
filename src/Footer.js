8920dd865a3c155c2b6182178dcfab803139c19e
Sawyer B <sawyerburnett@gmail.com>
2018-07-15 09:25:35 -0700

    adding preferences modal component. update hrefs with _blank to use rel=noreferer to further secure application

--- src/Footer.js
+++ src/Footer.js
@@ -7,9 +7,9 @@ class Footer extends React.Component {
 			<footer className="fixed-bottom text-center bg-dark text-light">
 				<div className="containter"> Created by Sawyer Burnett
 					<br/>
-					<a href="http://github.com/burnett2k" target="_blank">GitHub</a>
+					<a href="http://github.com/burnett2k" target="_blank" rel="noopener noreferrer">GitHub</a>
 					<br/>
-					<a href="http://linkedin.com/in/sawyer-burnett-9176411b" target="_blank">LinkedIn</a>
+					<a href="http://linkedin.com/in/sawyer-burnett-9176411b" target="_blank" rel="noopener noreferrer">LinkedIn</a>
 				</div>
 			</footer>
 		);
