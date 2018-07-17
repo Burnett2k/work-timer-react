f3cf6e221661b7d72413737ac6de5246ffbb23f3
Sawyer B <sawyerburnett@gmail.com>
2018-07-16 18:56:49 -0700

    adding modal event handlers to app.js since I cannot use bootstrap modal any more

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
+import ReactModal from 'react-modal';
 import './App.css';
 import Timer from './Timer.js';
 import Preferences from './Preferences.js';
 import Footer from './Footer.js';
 import Controls from './Controls.js';
 import SessionCounter from './SessionCounter.js';
+import PreferencesModal from './PreferencesModal.js';
+
+ReactModal.setAppElement('#root');
 
 class App extends Component {
+
+  constructor () {
+    super();
+    this.state = {
+      showModal: false
+    }
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal () {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal () {
+    this.setState( {showModal: false });
+  }
+
   render() {
     return (
       <div>
-        <Preferences />
+        <Preferences onClick={this.handleOpenModal}/>
         <div className="container text-center">
           <h4>POMODORO TIMER</h4>
           <Timer />
@@ -18,6 +41,7 @@ class App extends Component {
           <SessionCounter />
         </div>
         <Footer />
+        <PreferencesModal />
       </div>
     );
   }
