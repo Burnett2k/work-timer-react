2ca029172f45c5ea3d9ced75069cf0803af2cc98
Sawyer B <sawyerburnett@gmail.com>
2018-07-21 07:20:57 -0700

    fixing modal layout. what a piece of garbage
    that was annoying to style!

--- src/PreferencesModal.js
+++ src/PreferencesModal.js
@@ -1,4 +1,5 @@
 const React = require('react');
+const pomodoroLink = "https://en.wikipedia.org/wiki/Pomodoro_Technique";
 
 class PreferencesModal extends React.Component {
 
@@ -8,28 +9,30 @@ class PreferencesModal extends React.Component {
               <div className="modal-header">
                 <h5 className="modal-title">Settings</h5>
                 <h5 className="modal-title">
-                  <a href="https://en.wikipedia.org/wiki/Pomodoro_Technique" target="_blank" rel="noopener noreferrer">What is Pomodoro?</a>
+                  <a href={pomodoroLink} target="_blank" rel="noopener noreferrer">What is Pomodoro?</a>
                 </h5>
               </div>
               <div className="modal-body">
-                <div className="form-group row">
-                  <label for="minutes" className="col-form-label col-4">minutes (1-30)</label>
-                  <div className="col-4">
-                    <input id='minutes' type="number" className="form-control" name="minutes" min="1" max="30" value="25"></input>
+                  <div className="form-row q-top-buffer">
+                    <div className="col-6">
+                      <label htmlFor="minutes" className="col-form-label">minutes (1-30)</label>
+                    </div>
+                      <div className="col-4">
+                        <input id='minutes' type="number" className="form-control" name="minutes" min="1" max="30" defaultValue="25"></input>
+                      </div>
                   </div>
-                </div>
-                <div className="form-check">
-                  <input id="mute" className="form-check-input" type="checkbox" checked></input>
-                  <label for="mute" className="form-check-label">Mute?</label>
-                </div>
-                <div className="d-none d-sm-block d-md-block">
-                  <hr/>
-                  <h4>Keyboard Shortcuts</h4>
-                  <p> Start / Pause = F</p>
-                  <p> Stop          = S</p>
-                  <p> Reset         = R</p>
-                  <p> Preferences   = P</p>
-                </div>
+                  <div>
+                    <input id="mute" type="checkbox"></input>
+                    <label className="form-check-label" htmlFor="mute">Mute?</label>
+                  </div>
+              </div>      
+              <div className="d-none d-sm-block d-md-block">
+                <hr/>
+                <h4>Keyboard Shortcuts</h4>
+                <p> Start / Pause = F</p>
+                <p> Stop          = S</p>
+                <p> Reset         = R</p>
+                <p> Preferences   = P</p>
               </div>
               <div className="modal-footer">
                 <button id="save" type="button" className="btn btn-primary">Save</button>
