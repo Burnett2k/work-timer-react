73c931afb3fad409e4e0ed0d93b0c7ce8987538f
Sawyer B <sawyerburnett@gmail.com>
2018-08-22 12:33:23 -0700

    setting up event handlers for start, stop, reset buttons

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -5,9 +5,9 @@ class Controls extends React.Component {
 	render() {
 		return (
 			<div>
-				<button id="start" className="btn btn-lg btn-primary col-md-2" type="button">start</button>&nbsp; 
-				<button id="stop" className="btn btn-lg btn-primary col-md-2" type="button">stop</button>&nbsp;
-				<button id="reset" className="btn btn-lg btn-primary col-md-2" type="button">reset</button>
+				<button className="btn btn-lg btn-primary col-md-2" type="button" onClick={this.props.onStart}>start</button>&nbsp; 
+				<button className="btn btn-lg btn-primary col-md-2" type="button" onClick={this.props.onStop}>stop</button>&nbsp;
+				<button className="btn btn-lg btn-primary col-md-2" type="button" onClick={this.props.onReset}>reset</button>
 			</div>
 		);
 	}
--- src/containers/CountdownTimer.js
+++ src/containers/CountdownTimer.js
@@ -5,12 +5,36 @@ import Timer from '../components/Timer.js';
 
 class CountdownTimer extends React.Component {
 
+	constructor(props) {
+
+		super(props)
+
+		this.onStart = this.onStart.bind(this);
+		this.onStop = this.onStop.bind(this);
+		this.onReset = this.onReset.bind(this);
+
+	}
+
+	onStart() {
+		console.log("start");
+	}
+	onStop() {
+		console.log("stop");
+	}
+	onReset() {
+		console.log("reset");
+	}
+
 	render() {
 	  	return (
 			<div className="container text-center q-top-buffer">
       			<h4>POMODORO TIMER</h4>
 				<Timer />
-				<Controls />
+				<Controls 
+					onStart = {this.onStart}
+					onStop = {this.onStop}
+					onReset = {this.onReset}
+				/>
 				<SessionCounter />
 		    </div>);
 	}
