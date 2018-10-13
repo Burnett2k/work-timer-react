aabe0acf8f386b5d50dad3ef57592c6f65c5434b
Sawyer B <sawyerburnett@gmail.com>
2018-10-13 14:56:21 -0700

    add state for edit mode of goal

--- src/components/App.js
+++ src/components/App.js
@@ -1,83 +1,107 @@
-import React, { Component } from 'react';
-import './App.css';
-import CountdownTimer from '../containers/CountdownTimer.js';
-import Footer from './Footer.js';
-import SavePreferences  from '../containers/SavePreferences.js';
+import React, { Component } from "react";
+import "./App.css";
+import CountdownTimer from "../containers/CountdownTimer.js";
+import Footer from "./Footer.js";
+import SavePreferences from "../containers/SavePreferences.js";
+import SaveGoals from "../containers/SaveGoals";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       showModal: false,
       playPause: false,
       stop: false,
-      reset: false
-    }
+      reset: false,
+      isEditMode: false
+    };
 
     this.toggleModalShown = this.toggleModalShown.bind(this);
+    this.toggleEditMode = this.toggleEditMode.bind(this);
   }
 
   componentDidMount() {
-    document.addEventListener('keyup', this.handleKeyUp.bind(this));
+    document.addEventListener("keyup", this.handleKeyUp.bind(this));
   }
 
   toggleModalShown() {
-    this.setState(prevState => ({
-      showModal: !prevState.showModal
-    }));
+    if (!this.state.isEditMode) {
+      this.setState(prevState => ({
+        showModal: !prevState.showModal
+      }));
+    }
   }
 
   togglePlayPause() {
-    this.setState(prevState => ({
-      playPause: !prevState.playPause
-    }));
+    if (!this.state.isEditMode) {
+      this.setState(prevState => ({
+        playPause: !prevState.playPause
+      }));
+    }
   }
 
   toggleStop() {
-    this.setState(prevState => ({
-      stop: !prevState.stop
-    }));
+    if (!this.state.isEditMode) {
+      this.setState(prevState => ({
+        stop: !prevState.stop
+      }));
+    }
   }
 
   toggleReset() {
+    if (!this.state.isEditMode) {
+      this.setState(prevState => ({
+        reset: !prevState.reset
+      }));
+    }
+  }
+
+  toggleEditMode() {
     this.setState(prevState => ({
-      reset: !prevState.reset
+      isEditMode: !prevState.isEditMode
     }));
   }
 
   handleKeyUp(event) {
-    switch(event.key) {
-      case 'f':
+    switch (event.key) {
+      case "f":
         this.togglePlayPause();
         break;
-      case 'p':
+      case "p":
         this.toggleModalShown();
         break;
-      case 'r':
+      case "r":
         this.toggleReset();
         break;
-      case 's':
+      case "s":
         this.toggleStop();
+        break;
+      case "e":
+        this.toggleEditMode();
         break;
       default:
         break;
     }
   }
 
-render() {
-  return (
-    <div className='container q-top-buffer'>
-      <SavePreferences 
-        toggleModalShown={this.toggleModalShown}
-        showModal={this.state.showModal}
-      />
-      <CountdownTimer 
-        playPause={this.state.playPause}
-        stop={this.state.stop}
-        reset={this.state.reset}/>
-      <Footer />
-    </div>
+  render() {
+    return (
+      <div className="container q-top-buffer">
+        <SavePreferences
+          toggleModalShown={this.toggleModalShown}
+          showModal={this.state.showModal}
+        />
+        <CountdownTimer
+          playPause={this.state.playPause}
+          stop={this.state.stop}
+          reset={this.state.reset}
+        />
+        <SaveGoals
+          isEditMode={this.state.isEditMode}
+          toggleEditMode={this.toggleEditMode}
+        />
+        <Footer />
+      </div>
     );
   }
 }
