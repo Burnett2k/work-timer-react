daab1ba8b3f1507d534deae3b9d34e47d6d3bf53
Sawyer B <sawyerburnett@gmail.com>
2018-10-13 14:59:02 -0700

    Add 2 new components
    The goal link component which is what the user clicks on
    to edit their goal. The Save Goals component is a
    container that handles all the events associated with saving a goal

---
+++ src/components/GoalLink.js
@@ -0,0 +1,30 @@
+const React = require("react");
+
+const styles = {
+  display: "inline-block",
+  verticalAlign: "baseline"
+};
+
+class GoalLink extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        {this.props.goalText && this.props.goalText.length > 0 ? (
+          <span>
+            <h4 style={styles}>Goal: {this.props.goalText}</h4>{" "}
+            <a style={styles} href="#" onClick={this.props.toggleEditMode}>
+              {" "}
+              edit
+            </a>
+          </span>
+        ) : (
+          <a href="#" onClick={this.props.toggleEditMode}>
+            Want to set a goal?
+          </a>
+        )}
+      </React.Fragment>
+    );
+  }
+}
+
+export default GoalLink;
