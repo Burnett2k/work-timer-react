f43ca56abfc582838df383f824a9965661d35ea1
Sawyer B <sawyerburnett@gmail.com>
2018-10-13 14:56:11 -0700

    use outline buttons instead of btn primary

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,16 +1,35 @@
-const React = require('react');
+const React = require("react");
 
 class Controls extends React.Component {
-
-	render() {
-		return (
-			<div>
-				<button className="btn btn-lg btn-primary col-md-2" type="button" onClick={this.props.onStart}>{this.props.playPauseText}</button>&nbsp;
-				<button className="btn btn-lg btn-primary col-md-2" type="button" onClick={this.props.onStop}>stop</button>&nbsp;
-				<button className="btn btn-lg btn-primary col-md-2" type="button" onClick={this.props.onReset}>reset</button>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <button
+          className="btn btn-lg btn-outline-primary col-md-2"
+          type="button"
+          onClick={this.props.onStart}
+        >
+          {this.props.playPauseText}
+        </button>
+        &nbsp;
+        <button
+          className="btn btn-lg btn-outline-danger col-md-2"
+          type="button"
+          onClick={this.props.onStop}
+        >
+          stop
+        </button>
+        &nbsp;
+        <button
+          className="btn btn-lg btn-outline-primary col-md-2"
+          type="button"
+          onClick={this.props.onReset}
+        >
+          reset
+        </button>
+      </div>
+    );
+  }
 }
 
-export default Controls;	
+export default Controls;
