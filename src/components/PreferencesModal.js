a1843de2cfd5ca05dfec21d026e1f95cc4cc4f34
Sawyer B <sawyerburnett@gmail.com>
2018-10-17 19:52:36 -0700

    code formatting, comment out goal prompt feature which hasn't been completed

--- src/components/PreferencesModal.js
+++ src/components/PreferencesModal.js
@@ -1,13 +1,12 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const React = require('react');
+const React = require("react");
 const pomodoroLink = "https://en.wikipedia.org/wiki/Pomodoro_Technique";
 
 class PreferencesModal extends React.Component {
-
   static propTypes = {
     onSavePreferences: PropTypes.func
-  }
+  };
 
   constructor(props) {
     super(props);
@@ -16,7 +15,7 @@ class PreferencesModal extends React.Component {
       minutes: this.props.minutes,
       isMuted: this.props.isMuted,
       isNotesPrompt: this.props.isNotesPrompt
-    }
+    };
 
     this.enterPressed = this.enterPressed.bind(this);
     this.handleMinutesChange = this.handleMinutesChange.bind(this);
@@ -40,63 +39,93 @@ class PreferencesModal extends React.Component {
   }
 
   handleMinutesChange(numMinutes) {
-      let value = parseInt(numMinutes.target.value, 10) || 0;
-      this.setState({minutes: parseInt(value, 10)});
+    let value = parseInt(numMinutes.target.value, 10) || 0;
+    this.setState({ minutes: parseInt(value, 10) });
   }
 
   handleMutedChange(muted) {
-      this.setState({isMuted: muted.target.value === "true"});
+    this.setState({ isMuted: muted.target.value === "true" });
   }
 
   handleNotesPromptChange(notesPrompt) {
-    this.setState({isNotesPrompt: notesPrompt.target.value === "true"});
-}
+    this.setState({ isNotesPrompt: notesPrompt.target.value === "true" });
+  }
 
   onClick() {
     this.setPreferences();
-    this.props.onPreferencesSaved({minutes: this.state.minutes, isMuted: this.state.isMuted, isNotesPrompt: this.state.isNotesPrompt});
-    this.props.onRequestClose();  
+    this.props.onPreferencesSaved({
+      minutes: this.state.minutes,
+      isMuted: this.state.isMuted,
+      isNotesPrompt: this.state.isNotesPrompt
+    });
+    this.props.onRequestClose();
   }
 
   enterPressed(event) {
     let key = event.keyCode || event.which;
-    if (key === 13) { this.onClick() };
+    if (key === 13) {
+      this.onClick();
+    }
   }
 
-	render() {
-		return (
-	        <div>
-              <div className="modal-header">
-                <h5 className="modal-title">Settings</h5>
-                <h5 className="modal-title">
-                  <a href={pomodoroLink} target="_blank" rel="noopener noreferrer">What is Pomodoro?</a>
-                </h5>
+  render() {
+    return (
+      <div>
+        <div className="modal-header">
+          <h5 className="modal-title">Settings</h5>
+          <h5 className="modal-title">
+            <a href={pomodoroLink} target="_blank" rel="noopener noreferrer">
+              What is Pomodoro?
+            </a>
+          </h5>
+        </div>
+        <div className="modal-body">
+          <div className="form-row q-top-buffer">
+            <div className="col-6">
+              <label htmlFor="minutes" className="col-form-label">
+                minutes (1-30)
+              </label>
+            </div>
+            <div className="col-4">
+              <input
+                type="text"
+                className="form-control"
+                name="minutes"
+                min="1"
+                max="30"
+                value={this.state.minutes}
+                onChange={this.handleMinutesChange}
+              />
+            </div>
+          </div>
+          <div className="form-row q-top-buffer">
+            <div className="col-6">
+              <label htmlFor="mute">mute?</label>
+            </div>
+            <div className="col-6">
+              <div className="form-check form-check-inline">
+                <input
+                  type="radio"
+                  className="form-check-input"
+                  value="true"
+                  checked={this.state.isMuted}
+                  onChange={this.handleMutedChange}
+                />
+                <label className="form-check-label">Yes</label>
               </div>
-              <div className="modal-body">
-                  <div className="form-row q-top-buffer">
-                    <div className="col-6">
-                      <label htmlFor="minutes" className="col-form-label">minutes (1-30)</label>
-                    </div>
-                    <div className="col-4">
-                      <input type="text" className="form-control" name="minutes" min="1" max="30" value={this.state.minutes} onChange={this.handleMinutesChange}></input>
-                    </div>
-                  </div>
-                  <div className="form-row q-top-buffer">
-                    <div className="col-6">
-                      <label htmlFor="mute">mute?</label>
-                    </div>
-                    <div className="col-6">
-                      <div className="form-check form-check-inline">
-                        <input type="radio" className="form-check-input" value="true" checked={this.state.isMuted} onChange={this.handleMutedChange}/>
-                        <label className="form-check-label">Yes</label>
-                      </div>
-                      <div className="form-check form-check-inline">
-                        <input type="radio" className="form-check-input" value="false" checked={!this.state.isMuted} onChange={this.handleMutedChange}/>
-                        <label className="form-check-label">No</label>
-                      </div>
-                    </div>  
-                  </div>
-                  <div className="form-row">
+              <div className="form-check form-check-inline">
+                <input
+                  type="radio"
+                  className="form-check-input"
+                  value="false"
+                  checked={!this.state.isMuted}
+                  onChange={this.handleMutedChange}
+                />
+                <label className="form-check-label">No</label>
+              </div>
+            </div>
+          </div>
+          {/* <div className="form-row">
                     <div className="col-6">
                       <label htmlFor="mute">Prompt for Notes?</label>
                     </div>
@@ -110,22 +139,29 @@ class PreferencesModal extends React.Component {
                         <label className="form-check-label">No</label>
                       </div>
                     </div>  
-                  </div>
-                 <div className="d-none d-sm-block d-md-block">
-                  <hr/>
-                  <h4>Keyboard Shortcuts</h4>
-                  <p> Start / Pause = F</p>
-                  <p> Stop          = S</p>
-                  <p> Reset         = R</p>
-                  <p> Preferences   = P</p>
-                </div>
-              </div>      
-              <div className="modal-footer">
-                <button id="save" type="button" className="btn btn-primary" onClick={ this.onClick }>Save</button>
-              </div>
-        	</div>
-		);
-	}
+                  </div> */}
+          <div className="d-none d-sm-block d-md-block">
+            <hr />
+            <h4>Keyboard Shortcuts</h4>
+            <p> Start / Pause = F</p>
+            <p> Stop = S</p>
+            <p> Reset = R</p>
+            <p> Preferences = P</p>
+          </div>
+        </div>
+        <div className="modal-footer">
+          <button
+            id="save"
+            type="button"
+            className="btn btn-primary"
+            onClick={this.onClick}
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default PreferencesModal;	
+export default PreferencesModal;
