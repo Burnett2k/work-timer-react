3278b03cb6000d43cc171f70b935285f646e5ecc
Sawyer B <sawyerburnett@gmail.com>
2018-10-20 06:21:41 -0700

    switch to bar chart

--- src/components/HistoryButton.js
+++ src/components/HistoryButton.js
@@ -8,7 +8,7 @@ class HistoryButton extends React.Component {
         className="btn btn-outline-primary"
         onClick={this.props.toggleChartVisible}
       >
-        <i className="fa fa-history d-block d-md-none" aria-hidden="true" />
+        <i className="fa fa-bar-chart d-block d-md-none" aria-hidden="true" />
         <span className="d-none d-md-block d-lg-block d-xl-block">
           {this.props.isChartVisible ? "Timer" : "History"}
         </span>
