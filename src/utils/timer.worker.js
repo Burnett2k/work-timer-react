3c401e514209105d767740269b721d0a2a34f9ff
Sawyer Burnett <sawyerburnett@gmail.com>
2019-03-13 19:02:09 -0700

    add ability to handle 2 timers and clear other timers

--- src/utils/timer.worker.js
+++ src/utils/timer.worker.js
@@ -1,5 +1,27 @@
+let interval = '';
+let flashInterval = '';
+
 self.addEventListener('message', function(e) {
-    let result = e.data;
-    let workerResult = 'Result: ' + result;
-    self.postMessage(workerResult);
+    switch (e.data) {
+        case 'start':
+            interval = setInterval(() => {
+                self.postMessage('tick');
+            }, 1000);
+            break;
+        case 'flash':
+            flashInterval = setInterval(() => {
+                self.postMessage('flash');
+            }, 1000);
+            break;
+        case 'clearTimer':
+            clearInterval(interval);
+            break;
+        case 'clearFlash':
+            clearInterval(flashInterval);
+            break;
+        default:
+            clearInterval(interval);
+            clearInterval(flashInterval);
+            break;
+    }
 });
