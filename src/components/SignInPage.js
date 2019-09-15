d87ca911c54e43d8ad4c737607f77fa81f34aeec
Sawyer Burnett <sawyerburnett@gmail.com>
2019-09-15 10:47:22 -0700

    add image for google sign in

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -1,10 +1,17 @@
 import './App.css';
+import SignInImage from '../sounds/google_sign_in.png';
 
 const React = require('react');
 
 class SignInPage extends React.Component {
     render() {
-        return <button> Sign in with Google</button>;
+        return (
+            <input
+                type="image"
+                src={SignInImage}
+                alt="Sign in with Google"
+            ></input>
+        );
     }
 }
 
