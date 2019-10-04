c9c9a04baa41ac4c311a9ff09fed7027623bf5ae
Sawyer Burnett <sawyerburnett@gmail.com>
2019-10-03 18:58:39 -0700

    adding debug config

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -6,11 +6,14 @@ const React = require('react');
 class SignInPage extends React.Component {
     render() {
         return (
-            <input
-                type="image"
-                src={SignInImage}
-                alt="Sign in with Google"
-            ></input>
+            <React.Fragment>
+                <input
+                    type="image"
+                    src={SignInImage}
+                    alt="Sign in with Google"
+                ></input>
+                <a href="http://localhost:8080/auth/google">Sign in</a>
+            </React.Fragment>
         );
     }
 }
