a709d374dac7e1d489d97aa2d3b7330b7e811cf0
Sawyer Burnett <sawyerburnett@gmail.com>
2019-10-06 19:37:34 -0700

    mongoose support

--- server/passport-setup.js
+++ server/passport-setup.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('./models/user');
 
 passport.use(
     new GoogleStrategy(
@@ -8,13 +9,38 @@ passport.use(
             clientSecret: process.env.CLIENT_SECRET,
             callbackURL: process.env.REDIRECT_URL,
         },
-        function(accessToken, refreshToken, profile, cb) {
-            // In this example, the user's Facebook profile is supplied as the user
-            // record.  In a production-quality application, the Facebook profile should
-            // be associated with a user record in the application's database, which
-            // allows for account linking and authentication with other identity
-            // providers.
-            return cb(null, profile);
+        async (accessToken, refreshToken, profile, done) => {
+            const currentUser = await User.findOne({
+                googleId: profile.id,
+            });
+
+            if (!currentUser) {
+                const newUser = await new User({
+                    firstName: profile.name.givenName,
+                    lastName: profile.name.familyName,
+                    screenName: profile.displayName,
+                    googleId: profile.id,
+                }).save();
+                if (newUser) {
+                    done(null, newUser);
+                }
+            } else {
+                done(null, currentUser);
+            }
         }
     )
 );
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id)
+        .then((user) => {
+            done(null, user);
+        })
+        .catch((e) => {
+            done(new Error(`Failed to deserialize an user. ${e}`));
+        });
+});
