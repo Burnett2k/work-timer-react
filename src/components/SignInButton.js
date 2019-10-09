7910a27b7f667a2327e6a41ffea1eedb6dedc003
Sawyer Burnett <sawyerburnett@gmail.com>
2019-10-08 18:56:48 -0700

    add some logic to sign in button to handle authentication

--- src/components/SignInButton.js
+++ src/components/SignInButton.js
@@ -2,23 +2,21 @@ const React = require('react');
 import PropTypes from 'prop-types';
 
 class SignInButton extends React.Component {
-    static propTypes = { toggleSignInPage: PropTypes.func };
+    static propTypes = {
+        toggleSignInPage: PropTypes.func,
+        authenticated: PropTypes.bool,
+    };
 
     render() {
+        const { authenticated } = this.props;
         return (
-            <button
-                type="button"
-                className="btn btn-outline-primary"
-                onClick={this.props.toggleSignInPage}
-            >
-                <i
-                    className="fa fa-user-circle d-block d-md-none"
-                    aria-hidden="true"
-                />
-                <span className="d-none d-md-block d-lg-block d-xl-block">
-                    Sign In
-                </span>
-            </button>
+            <div>
+                {authenticated ? (
+                    <a href="http://localhost:8080/auth/logout">Logout</a>
+                ) : (
+                    <a href="http://localhost:8080/auth/google">Login</a>
+                )}
+            </div>
         );
     }
 }
