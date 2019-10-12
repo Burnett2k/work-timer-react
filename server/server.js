97e5dcdb5acb35cea17f775effee6b3fddc8ba7a
Sawyer Burnett <sawyerburnett@gmail.com>
2019-10-12 11:43:46 -0700

    add catch to handle mongo error

--- server/server.js
+++ server/server.js
@@ -8,13 +8,18 @@ const cors = require('cors');
 require('./passport-setup');
 
 // connect to mongo
-mongoose.connect(
-    process.env.MONGO_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
         console.log('connected to mongo db');
-    }
-);
+    })
+    .catch((err) => {
+        console.log(`cannot connect to mongo: ${err}`);
+        process.exit(1);
+    });
 
 // Create a new Express application.
 var app = express();
