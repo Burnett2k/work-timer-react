377abb7bb5d863482d99e5050f080da4f67c597e
Sawyer Burnett <sawyerburnett@gmail.com>
2019-11-01 19:38:46 -0700

    fix callbackUrl

--- server/passport-setup.js
+++ server/passport-setup.js
@@ -7,7 +7,7 @@ passport.use(
         {
             clientID: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET,
-            callbackURL: '/auth/google/redirect',
+            callbackURL: '/api/auth/google/redirect',
         },
         async (accessToken, refreshToken, profile, done) => {
             const currentUser = await User.findOne({
