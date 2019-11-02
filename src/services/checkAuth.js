fc6ca8a54540c267aaac86420b587d78c911dc5e
Sawyer Burnett <sawyerburnett@gmail.com>
2019-11-01 19:00:12 -0700

    use /api/ prefix

--- src/services/checkAuth.js
+++ src/services/checkAuth.js
@@ -1,12 +1,15 @@
 export const checkAuth = async () => {
-    const response = await fetch('https://worktimer.xyz/auth/login/success', {
-        method: 'GET',
-        credentials: 'include',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Credentials': true,
-        },
-    });
+    const response = await fetch(
+        'https://worktimer.xyz/api/auth/login/success',
+        {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Credentials': true,
+            },
+        }
+    );
     return response;
 };
