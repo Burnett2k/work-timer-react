7218aff46a2a419c81850c75b71099a5d2af4423
Sawyer Burnett <sawyerburnett@gmail.com>
2019-11-11 14:29:50 -0700

    save redux state to localstorage

--- src/index.js
+++ src/index.js
@@ -10,10 +10,19 @@ import App from './components/App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 
+import { loadState, saveState } from './utils/localStorage';
+
+const persistedState = loadState();
+
 const store = createStore(
     rootReducer,
+    persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+    saveState(store.getState());
+});
 
 //pass in redux store so entire App has access
 ReactDOM.render(
