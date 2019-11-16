2c879da63a645b712b2300129f2154fba3e8ae54
Sawyer Burnett <sawyerburnett@gmail.com>
2019-11-16 12:49:23 -0700

    add frontend call to session summary endpoint

---
+++ src/services/getSessionSummary.js
@@ -0,0 +1,24 @@
+export const getSessionSummary = async () => {
+    // todo modify URL once new endpoint exists
+    // use env variables for base url
+    const response = await fetch(
+        `${process.env.REACT_APP_SERVER_BASE_URL}/sessions/summary`,
+        {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Credentials': true,
+            },
+            mode: 'cors',
+        }
+    );
+    const data = await response.json();
+
+    if (data.success && data.aggregate) {
+        return data.aggregate;
+    } else {
+        return null;
+    }
+};
