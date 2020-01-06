b4ff37a23cea31abbb8e868a31f3b1c73dacfcdc
Sawyer Burnett <sawyerburnett@gmail.com>
2020-01-05 18:47:44 -0700

    make session list scrollable and give it a max height

--- src/components/SessionList.js
+++ src/components/SessionList.js
@@ -16,38 +16,48 @@ class SessionList extends React.Component {
         const { isLoaded, items } = this.props;
 
         return (
-            <ul className="list-group q-top-buffer">
+            <React.Fragment>
                 Pomodoros completed:
-                {isLoaded === true &&
-                    items &&
-                    items.map((item, key) => (
-                        <div
-                            key={key}
-                            className="list-group-item list-group-item-action"
-                        >
-                            <div className="d-flex w-100 justify-content-between">
-                                <h5 className="mb-1">
-                                    Pomodoro {items.length - key}{' '}
-                                </h5>
+                <ul
+                    className="list-group q-top-buffer"
+                    style={{
+                        overflow: 'scroll',
+                        maxHeight: '500px',
+                        marginTop: '5px',
+                    }}
+                >
+                    {isLoaded === true &&
+                        items &&
+                        items.map((item, key) => (
+                            <div
+                                key={key}
+                                className="list-group-item list-group-item-action"
+                            >
+                                <div className="d-flex w-100 justify-content-between">
+                                    <h5 className="mb-1">
+                                        Pomodoro {items.length - key}{' '}
+                                    </h5>
+                                    <small>
+                                        {new Date(item.date).toLocaleString()}
+                                    </small>
+                                </div>
+                                <p className="mb-1">
+                                    Notes:{' '}
+                                    {item.notes.text &&
+                                    item.notes.text.length > 0
+                                        ? item.notes.text
+                                        : 'N/A'}
+                                </p>
+
                                 <small>
-                                    {new Date(item.date).toLocaleString()}
+                                    Total Time:{' '}
+                                    {(item.secondsElapsed / 60).toFixed(1)}{' '}
+                                    minute(s)
                                 </small>
                             </div>
-                            <p className="mb-1">
-                                Notes:{' '}
-                                {item.notes.text && item.notes.text.length > 0
-                                    ? item.notes.text
-                                    : 'N/A'}
-                            </p>
-
-                            <small>
-                                Total Time:{' '}
-                                {(item.secondsElapsed / 60).toFixed(1)}{' '}
-                                minute(s)
-                            </small>
-                        </div>
-                    ))}
-            </ul>
+                        ))}
+                </ul>
+            </React.Fragment>
         );
     }
 }
