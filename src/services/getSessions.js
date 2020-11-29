82e934ede7170241c1932c932c5c2b947c253b3d
sawyer burnett <sawyerburnett@gmail.com>
2020-11-29 14:30:24 -0700

    fixing todos

--- src/services/getSessions.js
+++ src/services/getSessions.js
@@ -1,6 +1,4 @@
 export const getSessions = async () => {
-  // todo modify URL once new endpoint exists
-  // use env variables for base url
   const response = await fetch(
     `${process.env.REACT_APP_SERVER_BASE_URL}/sessions`,
     {
