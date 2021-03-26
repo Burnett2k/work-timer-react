83a99362e249a7aab52825614a7323165d371971
sawyer burnett <sawyerburnett@gmail.com>
2021-03-26 07:00:05 -0700

    fix weird timer bug

--- src/containers/CountdownTimer.js
+++ src/containers/CountdownTimer.js
@@ -94,7 +94,7 @@ class CountdownTimer extends React.Component {
       this.onReset();
     }
     if (prevProps.playPause !== this.props.playPause) {
-      this.onStart(false);
+      this.onStart();
     }
     if (prevProps.reset !== this.props.reset) {
       this.onReset();
@@ -105,7 +105,7 @@ class CountdownTimer extends React.Component {
   }
 
   onStart(forceStart = false) {
-    if (forceStart === false) {
+    if (forceStart === true) {
       this.worker.postMessage('start');
       this.props.dispatch(saveStatus(STARTED));
       this.setState({ playPauseText: PAUSE });
