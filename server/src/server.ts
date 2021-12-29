cf15a2b8aa99c3cdfea7739d8d8ab548a1d186f6
Sawyer Burnett <sawyerburnett@gmail.com>
2021-12-28 18:41:22 -0700

    upgrade mongo (#84)

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,9 +10,7 @@ require('./passport-setup');
 
 try {
   mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    reconnectTries: 300,
+    connectTimeoutMS: 60000,
     serverSelectionTimeoutMS: 60000
   });
 } catch (error) {
